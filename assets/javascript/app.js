eea95de15078ba23f7b39e89830fa44129c8ee61
Michael Barachkov <mike.barachkov@outlook.com>
2019-06-01 21:43:25 -0700

    got all functionality working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,44 @@
+$(document).ready(function () {
+    start_timer();
+    function start_timer() {
+        var seconds_left = 120;
+        document.getElementById('timer').innerHTML = seconds_left;
+        var interval = setInterval(function () {
+            document.getElementById('timer').innerHTML = --seconds_left;
+            if (seconds_left == 0) {
+                onSubmit();
+                clearInterval(interval);
+            }
+        }, 1000);
+    }
+})
 
-
-
-
-
-function onSubmit(){
+function onSubmit() {
     var score = 0;
     var numOfQuestions = 5;
-    var answers = ["b", "c", "a", "c", "b"];   
+    var answers = ["b", "c", "a", "c", "b"];
 
     q1 = document.forms["quiz"]["q1"].value;
     q2 = document.forms["quiz"]["q2"].value;
     q3 = document.forms["quiz"]["q3"].value;
     q4 = document.forms["quiz"]["q4"].value;
     q5 = document.forms["quiz"]["q5"].value;
-    
-    
-    for(var i = 1; i <= numOfQuestions; i++){
-        if(eval("q" + i) == ""){
-            alert("you got question one wrong." + i);
+
+
+    for (var i = 1; i <= numOfQuestions; i++) {
+        if (eval("q" + i) == "") {
+            alert("you got question " + i + " wrong");
         }
     }
-    for(var i = 1; i <= numOfQuestions; i++){
-        if(eval("q" + i) == answers[i - 1]){
+    for (var i = 1; i <= numOfQuestions; i++) {
+        if (eval("q" + i) == answers[i - 1]) {
             score++;
         }
     }
-    
+
     $(".results").html("<h2>You Scored " + score + " points out of " + numOfQuestions + "</h2>");
-    
+    $("#timer").hide();
+
     return false;
 }
+
